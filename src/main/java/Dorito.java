eacf8fc4a1564d8c4833135f4f28a3ec62f18c75
quantin96 <ngjiaquan3@gmail.com>
2025-01-24 07:20:25 +0800

    Implemented echo feature

--- src/main/java/Dorito.java
+++ src/main/java/Dorito.java
@@ -1,12 +1,24 @@
+import java.util.Scanner;
+
 public class Dorito {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String logo = "_________\n"
                 + "\\       /\n"
                 + " \\     / \n"
                 + "  \\   /  \n"
                 + "   \\ /   \n";
-        System.out.println(logo + "\nHello! I'm Dorito >.<\n"
-                + "What can I do for you?\n"
-                + "\nBye. Remember to stay hydrated! >.<\n");
+        String exit = "bye";
+        System.out.println(logo + "\nHello! I'm Dorito  >.<\n"
+                + "What can I do for you?  o.o");
+        while (true) {
+            String echo = sc.nextLine();
+            if (echo.equals(exit)) {
+                System.out.println("Bye. Remember to stay hydrated!  >.<");
+                sc.close();
+                break;
+            }
+            System.out.println(echo + "  >.<");
+        }
     }
 }
