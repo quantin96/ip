76a6bca033b62a1ac732542e7e9483125df42275
quantin96 <ngjiaquan3@gmail.com>
2025-01-24 07:07:18 +0800

    Renamed chatbot and implemented greet and exit

---
+++ src/main/java/Dorito.java
@@ -0,0 +1,12 @@
+public class Dorito {
+    public static void main(String[] args) {
+        String logo = "_________\n"
+                + "\\       /\n"
+                + " \\     / \n"
+                + "  \\   /  \n"
+                + "   \\ /   \n";
+        System.out.println(logo + "\nHello! I'm Dorito >.<\n"
+                + "What can I do for you?\n"
+                + "\nBye. Remember to stay hydrated! >.<\n");
+    }
+}
