5f6e0f291b802c6607c00a15e55853bafdb49e26
quantin96 <ngjiaquan3@gmail.com>
2025-01-24 08:23:23 +0800

    Added Task class and implemented a checkbox for tasks

--- src/main/java/Dorito.java
+++ src/main/java/Dorito.java
@@ -8,36 +8,44 @@ public class Dorito {
                 + " \\     / \n"
                 + "  \\   /  \n"
                 + "   \\ /   \n";
-        String exit = "bye";
-        String list = "list";
-        String[] tasks = new String[100];
-        int task = 0;
+        Task[] tasks = new Task[100];
+        int taskNo = 0;
 
         System.out.println(logo + "\nHello! I'm Dorito  >.<\n"
                 + "What can I do for you?  0.0");
         while (true) {
             String echo = sc.nextLine();
-            if (echo.equals(exit)) {
+            if (echo.equals("bye")) {
                 System.out.println("Bye. Remember to stay hydrated!  >.<");
                 sc.close();
                 break;
             }
-            if (echo.equals(list)) {
-                if (task == 0) {
-                    System.out.println("No current tasks!  >.<");
-                }
-                for (int i = 0; i < task; i++) {
-                    if (i + 1 == task) {
-                        System.out.println((i + 1) + ". " + tasks[i] + "  0.0");
-                        break;
-                    }
-                    System.out.println((i + 1) + ". " + tasks[i]);
+            if (echo.equals("list")) {
+                System.out.println("Here are the tasks in your list:  0.0");
+                for (int i = 0; i < taskNo; i++) {
+                    Task task = tasks[i];
+                    System.out.println((i + 1) + ".[" + task.getStatusIcon() + "] " + task);
                 }
                 continue;
             }
-            tasks[task] = echo;
-            task++;
-            System.out.println("added: " + echo + "  >.<");
+            if (echo.startsWith("mark")) {
+                Task task = tasks[Integer.parseInt(echo.split(" ")[1]) - 1];
+                task.mark();
+                System.out.println("Nice! I've marked this task as done:  >.<");
+                System.out.println("[" + task.getStatusIcon() + "] " + task);
+                continue;
+            }
+            if (echo.startsWith("unmark")) {
+                Task task = tasks[Integer.parseInt(echo.split(" ")[1]) - 1];
+                task.unmark();
+                System.out.println("OK! I've marked this task as not done:  0.0");
+                System.out.println("[" + task.getStatusIcon() + "] " + task);
+                continue;
+            }
+            Task task = new Task(echo);
+            tasks[taskNo] = task;
+            taskNo++;
+            System.out.println("added: " + task + "  >.<");
         }
     }
 }
