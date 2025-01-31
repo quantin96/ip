f490e4045001cfb0f1b8c8c82fdeaf98912c5114
quantin96 <ngjiaquan3@gmail.com>
2025-01-31 13:49:01 +0800

    Fix coding standard

--- src/main/java/dorito/Deadline.java
+++ src/main/java/dorito/Deadline.java
@@ -3,7 +3,7 @@ package dorito;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-public class Deadline extends Task{
+public class Deadline extends Task {
 
     protected LocalDate by;
 
--- src/main/java/dorito/ToDo.java
+++ src/main/java/dorito/ToDo.java
@@ -1,6 +1,6 @@
 package dorito;
 
-public class ToDo extends Task{
+public class ToDo extends Task {
 
     public ToDo(String description) {
         super(description);
